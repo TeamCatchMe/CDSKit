a9712d78635828b91be29458433ceb96ca40e4c6
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-11-08 13:45:45 +0900

    [Chore] BaseColor 변수들 Public으로 변경(#5)

--- CDSKit/Source/Foundation/CDSBaseColor.swift
+++ CDSKit/Source/Foundation/CDSBaseColor.swift
@@ -36,131 +36,131 @@ public enum CDSBaseColor {
     
     //  MARK: - Brand Colors
     
-    static var pink100: UIColor {
+    public static var pink100: UIColor {
         return UIColor(hex: "#EA4579")
     }
 
-    static var pink200: UIColor {
+    public static var pink200: UIColor {
         return UIColor(hex: "#EC477B")
     }
 
-    static var pink210: UIColor {
+    public static var pink210: UIColor {
         return UIColor(hex: "#FF4882")
     }
 
-    static var pink300: UIColor {
+    public static var pink300: UIColor {
         return UIColor(hex: "#FF77A2")
     }
     
     //  MARK: - Greyscale Colors
     
-    static var white: UIColor {
+    public static var white: UIColor {
         return UIColor(hex: "#FFFFFF")
     }
 
-    static var gray500: UIColor {
+    public static var gray500: UIColor {
         return UIColor(hex: "#E7E7E7")
     }
     
-    static var gray410: UIColor {
+    public static var gray410: UIColor {
         return UIColor(hex: "#E4E4E4")
     }
 
-    static var gray400: UIColor {
+    public static var gray400: UIColor {
         return UIColor(hex: "#B7B7B7")
     }
 
-    static var gray310: UIColor {
+    public static var gray310: UIColor {
         return UIColor(hex: "#A5A5A5")
     }
 
-    static var gray300: UIColor {
+    public static var gray300: UIColor {
         return UIColor(hex: "#858585")
     }
 
-    static var gray200: UIColor {
+    public static var gray200: UIColor {
         return UIColor(hex: "#737373")
     }
     
-    static var gray100: UIColor {
+    public static var gray100: UIColor {
         return UIColor(hex: "#5E5E5E")
     }
     
-    static var gray50: UIColor {
+    public static var gray50: UIColor {
         return UIColor(hex: "#5C5C5C")
     }
 
     // MARK: - Background Colors
     
-    static var black100: UIColor {
+    public static var black100: UIColor {
         return UIColor(hex: "#1C1C1C")
     }
 
-    static var black200: UIColor {
+    public static var black200: UIColor {
         return UIColor(hex: "#2F2F2F")
     }
 
-    static var cdsBackground: UIColor {
+    public static var cdsBackground: UIColor {
         return UIColor(hex: "#4D4D4D")
     }
     
-    static var popupBackground: UIColor {
+    public static var popupBackground: UIColor {
         return UIColor(hex: "#000000", alpha: 0.8)
     }
     
     //  MARK: - Catchu-back Colors
     
-    static var catchu100: UIColor {
+    public static var catchu100: UIColor {
         return UIColor(hex: "#F6BC4C")
     }
 
-    static var catchu200: UIColor {
+    public static var catchu200: UIColor {
         return UIColor(hex: "#709CD2")
     }
 
-    static var catchu300: UIColor {
+    public static var catchu300: UIColor {
         return UIColor(hex: "#74BDE0")
     }
     
-    static var catchu400: UIColor {
+    public static var catchu400: UIColor {
         return UIColor(hex: "#B4DD5E")
     }
     
-    static var catchu500: UIColor {
+    public static var catchu500: UIColor {
         return UIColor(hex: "#1FBEDC")
     }
     
-    static var catchu600: UIColor {
+    public static var catchu600: UIColor {
         return UIColor(hex: "#FDAF19")
     }
 
-    static var catchu700: UIColor {
+    public static var catchu700: UIColor {
         return UIColor(hex: "#57BEB7")
     }
     
-    static var catchu800: UIColor {
+    public static var catchu800: UIColor {
         return UIColor(hex: "#9073B1")
     }
     
     // MARK: - Check Colors
-    static var red100: UIColor {
+    public static var red100: UIColor {
         return UIColor(hex: "#EA4545")
     }
     
-    static var blue100: UIColor {
+    public static var blue100: UIColor {
         return UIColor(hex: "#0078D4")
     }
     
     // MARK: - Hover Colors
-    static var hover100: UIColor {
+    public static var hover100: UIColor {
         return UIColor(hex: "#FCFCFC", alpha: 0.35)
     }
     
-    static var hover110: UIColor {
+    public static var hover110: UIColor {
         return UIColor(hex: "#FCFCFC", alpha: 0.50)
     }
     
-    static var hover200: UIColor {
+    public static var hover200: UIColor {
         return UIColor(hex: "#928D60")
     }
 }
