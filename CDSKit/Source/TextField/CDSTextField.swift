23d6ef9408bb4f9ef2ef06d2dc1b86b9ca4718ac
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-12-22 23:35:46 +0900

    [Del] 사용하지 않는 함수 삭제

--- CDSKit/Source/TextField/CDSTextField.swift
+++ CDSKit/Source/TextField/CDSTextField.swift
@@ -38,11 +38,5 @@ extension UITextField {
         self.leftView = paddingView
         self.leftViewMode = .always
     }
-    
-    func setRightPaddingPoints(_ amount:CGFloat){
-        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
-        self.rightView = paddingView
-        self.rightViewMode = .always
-    }
 }
 #endif
